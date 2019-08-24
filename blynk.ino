ce935a6c9576f475368470012cb64d82ea4a2bb5
vasu kukreja <vasukukreja1999@gmail.com>
2019-08-24 01:32:19 -0700

    Create blynk.ino
    this file only runs in arduino ide with blynk and ESP2368 libraries installed.

---
+++ blynk.ino
@@ -0,0 +1,27 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "hotspot";
+char pass[] = "87654321";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
